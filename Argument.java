573f7d98d3c1afdbba86b8abebc6b75b0b942141
SakshiKadlag <96300617+SakshiKadlag@users.noreply.github.com>
2022-02-28 13:56:43 +0530

    Add files via upload

---
+++ Argument.java
@@ -0,0 +1,12 @@
+//import java.util.Scanner;
+class Argument
+{
+    public static void main(String arg[])
+{
+    
+    System.out.println("command line arguments");
+    System.out.println("total arguments =" +arg.length);
+    for(int i=0;i<arg.length;i++)
+     System.out.println(arg[i]);
+}
+}
